e593f518ee0404f8df82659c14a2bd1c14c44678
Callum Beckwith <123callumb@gmail.com>
2022-06-29 22:45:30 +0100

    huh

--- components/Homepage/SplashSection.tsx
+++ components/Homepage/SplashSection.tsx
@@ -7,17 +7,27 @@ import ResourceEnum from "../../lib/enums/ResourceEnum";
 
 export default function SplashSection() {
     return <Container maxW={"6xl"} pos="relative">
-        <Flex mt={{ md: "28vh", base: "24vh" }}>
-            <Flex spacing={4} color="blackShade" borderLeft="6px black solid" ml={{ md: "15%", base: "5%" }} pl="6" pb="0" flexDirection="column">
-                <Text fontSize="56" fontWeight="600">Callum Beckwith</Text>
-                <Text fontSize="26" fontWeight="200" mt="-2">Software Developer</Text>
-                <NextLink href={ResourceHelper.CreateInternalURL(InternalLinkEnum.CVSummary)}>
-                    <Button maxW="max-content" bgColor="cyanShade1" size="sm" mt="4" >Contact me</Button>
-                </NextLink>
-            </Flex>
-            <Spacer />
-            <Flex w="50%">
-                <Image src={ResourceHelper.CreateResourceURL(ResourceEnum.IsometricSplash)} />
+        <Flex>
+            <Flex w="100%"
+                height="70vh"
+                color="blackShade"
+                flexDirection="row"
+                justifyContent="center">
+                <Flex flexDir="column" minW="420px" maxW="420px" justifyContent="center">
+                    <Box borderLeft="6px solid" borderColor="blackShade" pl="6" mb="10vh">
+                        <Text fontSize="48" fontWeight="700" >Callum Beckwith</Text>
+                        <Text fontSize="34" fontWeight="700" mt="-2">Software Developer</Text>
+                        <Text fontSize="16" my="4">
+                            Hey! I’m Callum, software developer from the north of England. Come have a look at some of my work and experience :)
+                        </Text>
+                        <NextLink href={ResourceHelper.CreateInternalURL(InternalLinkEnum.CVSummary)}>
+                            <Button maxW="max-content" bgColor="btnBlueShade" color="whiteShade" size="sm" mt="4" >Contact me</Button>
+                        </NextLink>
+                    </Box>
+                </Flex>
+                <Flex overflow="visible" maxW="50%">
+                    <Image minH="100%" src={ResourceHelper.CreateResourceURL(ResourceEnum.IsometricSplash)} />
+                </Flex>
             </Flex>
             {/* <Stack spacing={5} flexDirection="column" color="whiteShade" px="4" d={{ base: 'none', md: 'flex' }}>
                 <a href={ExternalLinkEnum.Twitter} target="_blank" rel="noreferrer">
@@ -34,8 +44,8 @@ export default function SplashSection() {
             </Stack> */}
         </Flex>
         <VStack pos="absolute" bottom="12" w="100%">
-            <Text color="whiteShade" fontSize="2xl" fontWeight="100">What I&apos;ve been up to...</Text>
-            <Icon as={FaArrowCircleDown} color="whiteShade" boxSize="6" />
+            <Text fontSize="xl" fontWeight="400">What I've been up to...</Text>
+            <Icon as={FaArrowCircleDown} boxSize="6" />
         </VStack>
     </Container>
 }
