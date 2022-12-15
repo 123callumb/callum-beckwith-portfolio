6662cac1b3edd10e699ceb85ccad293ee292bac3
Callum Beckwith <123callumb@gmail.com>
2022-12-15 21:57:56 +0000

    rm

--- lib/helpers/ResourceHelper.ts
+++ lib/helpers/ResourceHelper.ts
@@ -1,7 +1,6 @@
 import { InternalLinkEnum } from "../enums/LinkEnum";
 import ResourceEnum from "../enums/ResourceEnum";
 
-// This is such a faff that this is a thing, but it has to be until next js sort their routing out with static file generation.
 export default class ResourceHelper {
     public static CreateInternalURL(url: InternalLinkEnum): string {
         return `/${url}${url !== InternalLinkEnum.Home ? `.html` : ""}`;
