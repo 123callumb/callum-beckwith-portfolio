fda42f4b7386897381faf0c0e6d35dd561db8947
Callum Beckwith <123callumb@gmail.com>
2022-12-23 20:39:15 +0000

    not much

--- components/Work/SummaryHorizontalBox.tsx
+++ components/Work/SummaryHorizontalBox.tsx
@@ -1,8 +1,9 @@
-import { Flex, HStack, VStack, Text, Icon, Box, Divider } from "@chakra-ui/react";
+import { Flex, HStack, VStack, Text, Icon, Box, Divider, Center } from "@chakra-ui/react";
 import FeaturedProject from "../../lib/props/FeaturedProject";
 import FramerBox from "../Motion/FramerBox";
 import Image from 'next/image';
 import ColourPaletteEnum from "../../lib/enums/ColourPaletteEnum";
+import { TiArrowRight } from "react-icons/ti";
 
 export default function SummaryHorizontalBox(props: FeaturedProject) {
     return <FramerBox whileInView={{
@@ -26,39 +27,33 @@ export default function SummaryHorizontalBox(props: FeaturedProject) {
         <VStack w="100%" p={2} pl="4" alignItems={"start"} flex="1">
             <Text fontSize="22" fontWeight="600" w="100%">{props.name}</Text>
             <Text size={"sm"} fontWeight="300" w="100%">{props.desc}</Text>
-            <Flex>
-                <HStack w="100%">{props.technologyUsed.map((m, i) =>
-                    <Icon
-                        key={i}
-                        as={m}
-                        boxSize={9}
-                        color="white"
-                        bgColor={"#68D8C2"}
-                        p="6px"
-                        borderRadius={"6"}
-                    />)}
-                </HStack> 
-                <Divider orientation="vertical" mx="3" />
-                <HStack w="100%">{props.languagesUsed.map((m, i) =>
-                    <Icon
-                        key={i}
-                        as={m}
-                        boxSize={9}
-                        color="white"
-                        bgColor={"#68D8C2"}
-                        p="6px"
-                        borderRadius={"6"}
-                    />)}
-                </HStack>
-            </Flex>
-            <Flex flexDir={"column"} pt={2}>
-                <Text px="4" fontWeight={"600"}>View Details</Text>
-                <Box
-                    mt="2px"
-                    height={"4px"}
-                    w="100%"
-                    background={"linear-gradient(to right, #589EA6 0%, #68D8C3  61%, #589EA6  100%)"}
-                    boxShadow="2px 2px 3px rgba(0, 0, 0, 0.1)"></Box>
+            <HStack w="100%">{props.technologyUsed.map((m, i) =>
+                <Icon
+                    key={i}
+                    as={m}
+                    boxSize={9}
+                    color="white"
+                    bgColor={"#68D8C2"}
+                    p="6px"
+                    borderRadius={"6"}
+                />)}
+            </HStack>
+            <HStack w="100%">{props.languagesUsed.map((m, i) =>
+                <Icon
+                    key={i}
+                    as={m}
+                    boxSize={9}
+                    color="white"
+                    bgColor={"#68D8C2"}
+                    p="6px"
+                    borderRadius={"6"}
+                />)}
+            </HStack>
+            <Flex fontWeight={"500"} color={ColourPaletteEnum.DarkBlue} fontStyle={"italic"} alignItems="center">
+                <Text>
+                    View Details
+                </Text>
+                <Icon as={TiArrowRight} />
             </Flex>
         </VStack>
     </FramerBox>
