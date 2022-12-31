928129c1e1d00493fbd2c766d2694b0a4ce2149c
Callum Beckwith <123callumb@gmail.com>
2022-12-31 13:54:07 +0000

    ye

--- pages/work/you-learn.tsx
+++ pages/work/you-learn.tsx
@@ -8,12 +8,12 @@ import { InternalLinkEnum } from "../../lib/enums/LinkEnum";
 export default function YouLearn() {
     return <Layout breadcrumb="Work" useCircuit={true}>
         <Summary
-            name="Easy Learn"
-            desc="Easy Learn is one of my favourite ongoing projects to date.
+            name="YouLearn"
+            desc="YouLearn is one of my favourite ongoing projects to date.
             The goal is to deliver learning experiences for users to get to
-            grips with using online services and mobile apps. EasyLearn places
+            grips with using online services and mobile apps. YouLearn places
             the users in guided sandbox environments where they can learn
-            through realistic scenarios and experiences. EasyLearn also provides
+            through realistic scenarios and experiences. YouLearn also provides
             teachers with classroom management abilities, ease of access features
             for their learners, and performance analysis tools."
             featureImageURL={ResourceEnum.Showcase_YouLearn}
